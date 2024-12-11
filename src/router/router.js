3ef3df6e44e179349259945ec6d15c63a677a64e
UPIN-0583 <quochuystg2004@gmail.com>
2024-12-11 15:49:45 +0700

    Thêm cuộn lên đầu trang khi chuyển hướng

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { ROUTING_HOME, ROUTING_LOGIN, ROUTING_MOVIEDETAIL_NS, ROUTING_MOVIEDETAIL_UC, ROUTING_MOVIELIST_NS, ROUTING_MOVIELIST_UC, ROUTING_SIGNUP, ROUTING_THEATER_DETAIL, ROUTING_PROMOTIONLIST, ROUTING_INFO, ROUTING_TICKET_HISTORY, ROUTING_CONTACT_FROM, ROUTING_ORDER_PAYMENT } from "./path";
 import { HomePage } from "../page/Home.js";
 import { Layout } from "../page/layout.js";
@@ -15,12 +17,23 @@ import { TicketHistory } from "../page/TicketHistory.js";
 import { ContactForm } from "../page/ContactForm.js";
 import { OrderPayment } from "../page/OrderPayment.js";
 
+const ScrollToTop = () => {
+    const location = useLocation();
+  
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [location]);
+  
+    return null;
+  };
+
 
 
 const WebRouter = () => {
 
     return (
         <BrowserRouter>
+        <ScrollToTop />
             <Routes>
                 <Route path={""} element={<Layout />}>
                     {/* Trang mặc định */}
