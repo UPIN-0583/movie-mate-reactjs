6d031e95dc97d20015aba8019d155f1ff5c5a114
UPIN-0583 <quochuystg2004@gmail.com>
2024-12-11 16:52:05 +0700

    clean code

--- src/page/MovieListNS.js
+++ src/page/MovieListNS.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import { NowShowingMovieCard } from "../components/NowShowingMovieCard";
 import { useMovies } from "../context/MovieContext";
 
